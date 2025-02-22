5c06c7fbe69a2fb5d55aeee2007ca81c072ec50c
chimkenu <106415170+chimkenu@users.noreply.github.com>
2025-02-22 23:05:12 +0000

    now using sprites

---
+++ src/Sprite.js
@@ -0,0 +1,77 @@
+import "./Vector2";
+import { Vector2 } from "./Vector2";
+
+export class Sprite {
+  /**
+   * @description handles all sprite loading/rendering
+   * 
+   * @param {Object} param0 - all options
+   * @param {string} param0.resource - image to draw
+   * @param {number} param0.frameSize - size of crop of image
+   * @param {number} param0.hFrames - horizontal sprite arrangement
+   * @param {number} param0.vFrames - vertical sprite arrangement
+   * @param {number} param0.frame - which frame to display
+   * @param {number} param0.scale - size of image
+   * @param {*} param0.position - where in the canvas to draw (top left corner)
+   */
+  constructor({
+    resource,
+    frameSize,
+    hFrames,
+    vFrames,
+    frame,
+    scale,
+    position
+  }) {
+    this.resource = resource;
+    this.frameSize = frameSize ?? new Vector2(16, 16);
+    this.hFrames = hFrames ?? 1;
+    this.vFrames = vFrames ?? 1;
+    this.frame = frame ?? 0;
+    this.frameMap = new Map();
+    this.scale = scale ?? 1;
+    this.position = position ?? new Vector2();
+    this.buildFrameMap();
+  }
+
+  buildFrameMap() {
+    let frameCount = 0;
+    for (let v = 0; v < this.vFrames; v++) {
+      for (let h = 0; h < this.hFrames; h++) {
+        this.frameMap.set(
+          frameCount,
+          new Vector2(h * this.frameSize.x, v * this.frameSize.y)
+        );
+        frameCount++;
+      }
+    }
+  }
+
+  drawImage(ctx, x, y) {
+    if (!this.resource.isLoaded) {
+      return;
+    }
+
+    x = x ?? this.position.x;
+    y = y ?? this.position.y;
+
+    // find the correct sprite sheet frame to use
+    const frame = this.frameMap.get(this.frame);
+    if (frame == null) {
+      console.log(`ERROR: INVALID FRAME '${this.frame}' ON ${this}`);
+      return;
+    }
+
+    ctx.drawImage(
+      this.resource.image,
+      frame.x,
+      frame.y,
+      this.frameSize.x,
+      this.frameSize.y,
+      x,
+      y,
+      this.frameSize.x * this.scale,
+      this.frameSize.y * this.scale
+    );
+  }
+}
